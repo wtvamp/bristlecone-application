d2a2056694713fda209975eb3a9927144977b48d
Warren Thompson <warrenjthompson@gmail.com>
2017-02-04 12:35:46 -0800

    Adding first crack at biz layer

---
+++ src/Bristlecone.BizLogicLayer/Common/BusinessEntity.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using Bristlecone.DataLayer.Common;
+using Bristlecone.DataLayer.Interfaces;
+
+namespace Bristlecone.BusinessLayer.Common
+{
+    public abstract class BusinessEntity<T> : IBusinessEntity<T> where T : BaseEntity
+    {
+        IGenericRepository<T> _repository;
+
+        protected BusinessEntity(IGenericRepository<T> repository)
+        {
+            _repository = repository;
+        }
+
+        public void Create(T entity)
+        {
+            if (entity == null)
+            {
+                throw new ArgumentNullException(nameof(entity));
+            }
+            _repository.Add(entity);
+        }
+
+        public void Delete(T entity)
+        {
+            if (entity == null) throw new ArgumentNullException(nameof(entity));
+            _repository.Delete(entity);
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return _repository.GetAll();
+        }
+
+        public void Update(T entity)
+        {
+            if (entity == null) throw new ArgumentNullException(nameof(entity));
+            _repository.Edit(entity);
+        }
+    }
+}
---
+++ src/Bristlecone.BizLogicLayer/Interfaces/IApplicationBusinessEntity.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+using Bristlecone.BusinessLayer.Common;
+using Bristlecone.DataLayer.Entities;
+
+namespace Bristlecone.BusinessLayer.Interfaces
+{
+
+    public interface IApplicationBusinessEntity : IBusinessEntity<Application>
+    {
+        Task<Application> GetApplicationAsync(long id);
+
+        Task<Application> CreateApplicationAsync(Application Application);
+
+        Task<Application> UpdateApplicationAsync(Application Application);
+    }
+
+}
