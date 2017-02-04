094d19d53da4cb29a44969ccddb48e5a26344f1e
Warren Thompson <warrenjthompson@gmail.com>
2017-02-04 12:49:11 -0800

    Added view model for application and viewmodels project

---
+++ src/Bristlecone.ViewModels/DTO/ApplicationViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Bristlecone.ViewModels.DTO
+{
+    public class ApplicationDTO
+    {
+        public int ApplicationID { get; set; }
+
+        public string ApplicationName { get; set; }
+
+        public string ApplicationType { get; set; }
+    }
+}
